588ccb579c118e8f38c1eece50113d4f5a84b916
Влад Устинов <lvpd.help@gmail.com>
2023-10-17 15:00:23 +0300

    vrrrrrrer

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-import { greeting } from "./math.js";
-console.log(greeting());
+import express from "express";
+const app = express();
+app.listen(3000, () => {
+  console.log(`Сервер прослушивается на порте 3000`);
+});
+app.get(`/as/`, function (req, res) {
+  res.send(`Hello peedoor`);
+  console.log(req.cookies);
+});
+// import path from "path";
+// import { fileURLToPath } from "node:url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
