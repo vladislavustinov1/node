3dfe432c630907f935fc20527f7e0fa7482de128
Влад Устинов <lvpd.help@gmail.com>
2023-10-27 15:13:34 +0300

    frer

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-import express from "express";
+const express = require(`express`);
+const favicon = require(`express-favicon`);
+const path = require(`path`);
+const fs = require("fs");
 const app = express();
-app.listen(3000, () => {
-  console.log(`Сервер прослушивается на порте 3000`);
+let date = new Date();
+app.use(express.static(path.join(__dirname, `public`)));
+console.log(__dirname + "/public/1.png");
+app.use(favicon(__dirname + "/public/1.png"));
+const port = "3000";
+app.get("/", function (req, res) {
+  fs.writeFile("1.txt", `Клиент зашёл на сервер в ${date}`);
+  res.sendFile(__dirname + "/public/index.html");
 });
-app.get(`/as/`, function (req, res) {
-  res.send(`Hello peedoor`);
+app.listen(port, () => {
+  console.log(`Сервер прослушивается на порте ` + port);
 });
-app.post("/as/", function (req, res) {
-  console.log(req.body);
-});
-// import path from "path";
-// import { fileURLToPath } from "node:url";
+// app.post("/as/", function (req, res) {
+//   console.log(req.body);
+// });
 
+// import { fileURLToPath } from "node:url";
+// import express from "express";
+// import favicon from "express-favicon";
+// // import path from "path";
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
